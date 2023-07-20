3e6662cb75a4db1cbe200fe5370319892816fb06
hrdjava05 <hrdjava05@gmail.com>
2023-07-20 09:55:48 +0900

    first commit

---
+++ src/main/java/org/spring/ajaxpj/AjaxpjApplication.java
@@ -0,0 +1,13 @@
+package org.spring.ajaxpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxpjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/spring/ajaxpj/AjaxpjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.ajaxpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxpjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
